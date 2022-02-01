306f9e7cfc62185d4856a9e752e064795fbdeb63
Steelheart96 <wol21014@byui.edu>
2022-01-31 16:10:27 -0800

    Fixed Ham Menu Bug - Menu would not reset 'X' after "onresize" went into effect.

--- chamber/js/responsive.js
+++ chamber/js/responsive.js
@@ -9,25 +9,17 @@ menubutton.onclick = () => {
     ex.classList.toggle('responsive')
 };
 
-
-// window.onresize = () => {
-//     if(window.innerWidth > 1000){
-//     firstNav.classList.remove('open');
-//     button.classList.remove('open')
-//     }
-// }
-
 let buttonMaxWidth = 1199;
 
 window.onresize = () => {
     if (window.innerWidth < buttonMaxWidth){
         pagenav.classList.add('responsive');
+        ham.classList.remove('responsive');
+        ex.classList.add('responsive')
     };
     if (window.innerWidth > buttonMaxWidth){
         pagenav.classList.remove('responsive');
+        ham.classList.remove('responsive');
+        ex.classList.add('responsive')
     };
 };
-
-// window.addEventListener('resize', () => {
-//     if (window.innerWidth > 760) pagenav.classList.remove('responsive')
-// }, true);
