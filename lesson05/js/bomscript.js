2c5398d0cec86a325330335117dfda59c40a373a
Steelheart96 <wol21014@byui.edu>
2022-02-02 09:21:23 -0800

    Added Week05 Activity - Added folders and files for Week05 - Added Week05 Activity to appropriate folders

---
+++ lesson05/js/bomscript.js
@@ -0,0 +1,21 @@
+const usrInput = document.querySelector("input");
+const btnInput = document.querySelector(".add");
+const listOutput = document.querySelector(".list");
+
+btnInput.addEventListener("click", () => {
+	if (usrInput.value != "") {
+		let newLi = document.createElement("li");
+		let showInput = document.createElement("h1");
+		showInput = usrInput.value;
+
+		let exbtn = document.createElement("button");
+		exbtn.textContent = "❌";
+
+		exbtn.addEventListener("click", () => {listOutput.removeChild(newLi);});
+
+		newLi.append(showInput, exbtn);
+		listOutput.appendChild(newLi);
+        
+		usrInput.value = "";
+	}
+});
